3454586bcbdddb8046e2a23254fc7f2212e083b9
Brandon <brandonaguerodeveloper@gmail.com>
2023-12-14 13:07:08 -0500

    Finalized styling and content for the header component

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Route } from '@models/route.model';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  routesList = signal<Route[]>([
+    {
+      id: 1,
+      path: '/our-company',
+      label: 'Our Company',
+    },
+    {
+      id: 2,
+      path: '/locations',
+      label: 'Locations',
+    },
+    {
+      id: 3,
+      path: '/contact',
+      label: 'Contact',
+    },
+  ]);
+}
