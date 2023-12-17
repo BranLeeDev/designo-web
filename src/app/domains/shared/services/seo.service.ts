44acf6273e08fa7e2c4a9e22231ef95345c52ee7
Brandon <brandonaguerodeveloper@gmail.com>
2023-12-17 13:01:48 -0500

    Added SEO service with constructor and method, also added TagsConfig model with four string properties: title, description, image, and slug

---
+++ src/app/domains/shared/services/seo.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import { TagsConfig } from '@models/tags-config.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SeoService {
+  constructor(private meta: Meta) {}
+
+  generateTags(config: TagsConfig) {
+    this.meta.updateTag({
+      name: 'description',
+      content: config.description,
+    });
+
+    this.meta.updateTag({
+      property: 'og:url',
+      content: `https://designo-web.web.app/${config.slug}`,
+    });
+
+    this.meta.updateTag({
+      property: 'og:title',
+      content: config.title,
+    });
+
+    this.meta.updateTag({
+      property: 'og:description',
+      content: config.description,
+    });
+
+    this.meta.updateTag({
+      property: 'og:image',
+      content: config.image,
+    });
+
+    this.meta.updateTag({
+      property: 'twitter:url',
+      content: `https://designo-web.web.app/${config.slug}`,
+    });
+
+    this.meta.updateTag({
+      name: 'twitter:title',
+      content: config.title,
+    });
+
+    this.meta.updateTag({
+      name: 'twitter:description',
+      content: config.description,
+    });
+
+    this.meta.updateTag({
+      name: 'twitter:image',
+      content: config.image,
+    });
+  }
+}
