211de85ec9c60bb354b0af4f0ee3f0cb63ead385
Brandon <brandonaguerodeveloper@gmail.com>
2023-12-19 12:51:14 -0500

    Created the 'Introducing' component within the 'home' domain

---
+++ src/app/domains/home/components/introducing/introducing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-introducing',
+  standalone: true,
+  imports: [],
+  templateUrl: './introducing.component.html',
+  styleUrl: './introducing.component.scss',
+})
+export class IntroducingComponent {}
