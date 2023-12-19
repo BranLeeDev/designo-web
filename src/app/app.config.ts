0fb626f088a40b2e571fdcef98ddfb6afd883f44
Brandon <brandonaguerodeveloper@gmail.com>
2023-12-18 21:11:49 -0500

    Added @angular/pwa using the 'ng add' command to enable progressive web app features

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import {
   PreloadAllModules,
   provideRouter,
@@ -7,7 +7,11 @@ import {
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withPreloading(PreloadAllModules)), provideClientHydration()],
+  providers: [provideRouter(routes, withPreloading(PreloadAllModules)), provideClientHydration(), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })],
 };
