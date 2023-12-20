47828e1bc52e09f0494e536a1cb112a227d8091f
Brandon <brandonaguerodeveloper@gmail.com>
2023-12-19 20:19:44 -0500

    Created the 'CultureCard' component within the 'home' domain

---
+++ src/app/domains/home/components/culture-card/culture-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-culture-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './culture-card.component.html',
+  styleUrl: './culture-card.component.scss',
+})
+export class CultureCardComponent {}
