6c5af171dd4879c32318b2566bc0a5717a919965
Brandon <brandonaguerodeveloper@gmail.com>
2023-12-21 14:24:32 -0500

    Changed background generation from the footer component to the talk-card component. Now, the background will be generated only if it receives a true input

--- src/app/domains/shared/components/talk-card/talk-card.component.ts
+++ src/app/domains/shared/components/talk-card/talk-card.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-talk-card',
   standalone: true,
   imports: [],
   templateUrl: './talk-card.component.html',
-  styleUrl: './talk-card.component.scss'
+  styleUrl: './talk-card.component.scss',
 })
 export class TalkCardComponent {
-
+  @Input() hasBackground: boolean = true;
 }
