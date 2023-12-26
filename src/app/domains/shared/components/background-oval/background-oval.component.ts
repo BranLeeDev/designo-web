9b83d6614f4b719028571a091422f58430db3db8
Brandon <brandonaguerodeveloper@gmail.com>
2023-12-26 12:38:24 -0500

    Added background-oval component in the shared domain, added initial styles for the project-intro component

---
+++ src/app/domains/shared/components/background-oval/background-oval.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background-oval',
+  standalone: true,
+  imports: [],
+  templateUrl: './background-oval.component.html',
+  styleUrl: './background-oval.component.scss'
+})
+export class BackgroundOvalComponent {
+
+}
--- src/app/domains/shared/components/talk-card/talk-card.component.ts
+++ src/app/domains/shared/components/talk-card/talk-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BackgroundOvalComponent } from '../background-oval/background-oval.component';
 
 @Component({
   selector: 'app-talk-card',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, BackgroundOvalComponent],
   templateUrl: './talk-card.component.html',
   styleUrl: './talk-card.component.scss',
 })
