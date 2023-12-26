ab8c006b7c63e3ab59eb7cd6212fa14ac41ccc82
Brandon <brandonaguerodeveloper@gmail.com>
2023-12-26 13:39:09 -0500

    Added the background-oval component to the project-intro component, removed the border-radius for the .project-intro class for measurements greater than 48em, within the project-intro component

--- src/app/domains/home/components/project-intro/project-intro.component.ts
+++ src/app/domains/home/components/project-intro/project-intro.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { ProjectIntro } from '@models/project-intro.model';
+import { BackgroundOvalComponent } from '../../../shared/components/background-oval/background-oval.component';
 
 @Component({
   selector: 'app-project-intro',
   standalone: true,
-  imports: [],
+  imports: [BackgroundOvalComponent],
   templateUrl: './project-intro.component.html',
   styleUrl: './project-intro.component.scss',
 })
