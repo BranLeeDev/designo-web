6055f9d914a3bf27deefcc00359483efc1e6a12c
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-04 08:23:55 -0500

    Uninstalled the leaflet library and @types/leaflet. Removed all related components, styles, and configurations

--- src/app/domains/locations/components/location-card/location-card.component.ts
+++ src/app/domains/locations/components/location-card/location-card.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { LocationMapComponent } from '../location-map/location-map.component';
 import { LocationCard } from '@models/location-card.model';
 import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-location-card',
   standalone: true,
-  imports: [LocationMapComponent, NgClass],
+  imports: [NgClass],
   templateUrl: './location-card.component.html',
   styleUrl: './location-card.component.scss',
 })
